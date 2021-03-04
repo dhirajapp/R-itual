6a847ad41e31d1e5eb7bbb834d32defb70f73b40
Dhiraj Kumar <dhirajkumar31298@gmail.com>
2021-03-05 05:00:05 +0530

    added the signing

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,39 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { signin } from '../actions/userActions';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
 
-export default function SigninScreen(props) {
+export default function SigninScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const redirect = props.location.search
-    ? props.location.search.split('=')[1]
-    : '/';
-
-  const userSignin = useSelector((state) => state.userSignin);
-  const { userInfo, loading, error } = userSignin;
-
-  const dispatch = useDispatch();
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(signin(email, password));
+    // TODO: sign in action
   };
-  useEffect(() => {
-    if (userInfo) {
-      props.history.push(redirect);
-    }
-  }, [props.history, redirect, userInfo]);
   return (
     <div>
       <form className="form" onSubmit={submitHandler}>
         <div>
           <h1>Sign In</h1>
         </div>
-        {loading && <LoadingBox></LoadingBox>}
-        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email address</label>
           <input
@@ -63,10 +43,7 @@ export default function SigninScreen(props) {
         <div>
           <label />
           <div>
-            New customer?{' '}
-            <Link to={`/register?redirect=${redirect}`}>
-              Create your account
-            </Link>
+            New customer? <Link to="/register">Create your account</Link>
           </div>
         </div>
       </form>
