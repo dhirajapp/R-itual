106b56d726aa9da2397e26e0c9d8dfe96dfb8f25
Dhiraj Kumar <dhirajkumar31298@gmail.com>
2021-03-09 10:10:38 +0530

    shiping address

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -22,7 +22,7 @@ export default function ShippingAddressScreen(props) {
     dispatch(
       saveShippingAddress({ fullName, address, city, postalCode, country })
     );
-    props.history.push('/payment');
+    props.history.push('/https://test.instamojo.com/@dhirajkumar31298');
   };
   return (
     <div>
